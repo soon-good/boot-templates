3d641e91c14879a38162ba09f02329bf0050fb9c
gosgjung <playmymind@daum.net>
2021-07-02 09:08:35 +0900

    디렉터리 정리

--- single_module/src/main/java/io/study/qdsl/single_module/company/employee/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package io.study.qdsl.single_module.company.employee;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
